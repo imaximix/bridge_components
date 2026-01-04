7df5abc3bb7498dffad2d12352f8e10f8c7a504e
Maximilian Babescu <maximilian.babesc@gmail.com>
2026-01-04 22:10:01 +0200

    Export controllers from index.js

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -3,3 +3,7 @@ import TabBarController from "./tab_bar_controller.js";
 export {
   TabBarController
 };
+
+export const controllers = [
+  { identifier: "tab-bar",  controllerConstructor: TabBarController }
+];
