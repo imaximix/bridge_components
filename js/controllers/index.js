9360d09f2a4952bfb587abe4f23f82f3f6e62408
Maximilian Babescu <maximilian.babesc@gmail.com>
2026-07-31 15:17:09 +0300

    Add js button controller

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,12 +1,15 @@
 import TabBarController from "./tab_bar_controller.js";
 import SecretsStoreController from "./secrets_store_controller.js";
+import ButtonController from "./button_controller.js";
 
 export {
   TabBarController,
-  SecretsStoreController
+  SecretsStoreController,
+  ButtonController
 };
 
 export const controllers = [
   { identifier: "tab-bar",  controllerConstructor: TabBarController },
-  { identifier: "secrets-store", controllerConstructor: SecretsStoreController }
+  { identifier: "secrets-store", controllerConstructor: SecretsStoreController },
+  { identifier: "button", controllerConstructor: ButtonController }
 ];
